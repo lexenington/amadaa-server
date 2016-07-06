bffc2231d2115293848bcd41a803797fbbadd026
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-06 13:57:07 +0000

    Created AmadaaServer class. Closes #1

---
+++ amadaa/server.py
@@ -0,0 +1,14 @@
+class AmadaaServer:
+	"""" The main Amadaa server. It is responsible for starting up and
+	managing all the other parts. """
+	
+	def __init__(self):
+		pass
+		
+	def start(self):
+		"""" Start the server """
+		pass
+		
+	def stop(self):
+		"""" Stop the server """
+		pass
