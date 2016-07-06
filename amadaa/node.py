75055464f6c3123761c260f4bdaf21cc163bfb54
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-06 16:29:49 +0000

    Created amadaa.node. Closes #9

---
+++ amadaa/node.py
@@ -0,0 +1,7 @@
+def connection_change(state):
+	if state == True:
+		# TODO: something better here
+		print("node was told connection is up")
+	else:
+		# TODO: something better here
+		print("node was told connection is down")
