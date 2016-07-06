0f69663697563039de09ee4eda7f1071849884c7
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-06 17:23:08 +0000

    Start/stop cherrypy when amadaa.server starts/stops. Closes #3

--- amadaa/server.py
+++ amadaa/server.py
@@ -1,8 +1,10 @@
 import sys
 import time
 import schedule
+import cherrypy
 import amadaa.connection
 import amadaa.node
+from amadaa.root.controller import RootController
 
 this = sys.modules[__name__]
 this.running = False
@@ -11,13 +13,15 @@ def start():
 	this.running = True
 	amadaa.connection.add_observer(amadaa.node.connection_change)
 	schedule.every(1).minutes.do(amadaa.connection.check_internet_connection)
+	cherrypy.tree.mount(RootController(), "/")
+	cherrypy.engine.start()
 	while this.running:
 		schedule.run_pending()
 		time.sleep(1)
 	stop()
 	
 def stop():
-	pass
+	cherrypy.engine.exit()
 	
 def sighandler(signum, frame):
 	this.running = False
