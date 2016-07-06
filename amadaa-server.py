90e70f21367551a96ea9487ab1cba2d97019d2bf
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-06 23:00:59 +0000

    Change to this directory before launching server. Closes #14

--- amadaa-server.py
+++ amadaa-server.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python3
 
+import os
 import signal
 import amadaa.server
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 signal.signal(signal.SIGTERM, amadaa.server.sighandler)
 signal.signal(signal.SIGINT, amadaa.server.sighandler)
 amadaa.server.start()
