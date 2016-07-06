9901ad45e770eb7aeb2ed411d26848a3849e6cab
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-06 15:46:22 +0000

    Created amadaa.connection. Closes #5

---
+++ amadaa/connection.py
@@ -0,0 +1,26 @@
+import sys
+import socket
+
+this = sys.modules[__name__]
+this.state = None
+this.observers = []
+
+def check_internet_connection():
+	"""" Check if we are connected to the Internet and inform observers
+	of any change in connectivity. """
+	try:
+		socket.setdefaulttimeout(3)
+		socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
+		newstate = True
+	except:
+		newstate = False
+	if not newstate == this.state:
+		for ob in observers:
+			ob(newstate)
+	this.state = newstate	
+	
+def add_observer(ob):
+	"""" Add an observer.
+	:param ob: the function to be called if internet connectivity goes up/down. """
+	if callable(ob):
+		this.observers.append(ob)
