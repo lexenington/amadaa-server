695ec742e78f66294997c4ff05456e10bef0781d
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-06 14:57:03 +0000

    Shutdown on SIGTERM and SIGINT. Closes #7

--- amadaa-server.py
+++ amadaa-server.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 
+import signal
 import amadaa.server
 
+signal.signal(signal.SIGTERM, amadaa.server.sighandler)
+signal.signal(signal.SIGINT, amadaa.server.sighandler)
 amadaa.server.start()
--- amadaa/server.py
+++ amadaa/server.py
@@ -1,5 +1,18 @@
+import sys
+import time
+
+this = sys.modules[__name__]
+this.running = False
+
 def start():
-	pass
+	this.running = True
+	while this.running:
+		time.sleep(1)
+	stop()
 	
 def stop():
 	pass
+	
+def sighandler(signum, frame):
+	this.running = False
+	
