1cf0987718c2daeecffd15785a59070f0425521f
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-06 16:35:45 +0000

    Let amadaa.node observe connection changes. Closes #11

--- amadaa/server.py
+++ amadaa/server.py
@@ -1,12 +1,16 @@
 import sys
 import time
 import schedule
+import amadaa.connection
+import amadaa.node
 
 this = sys.modules[__name__]
 this.running = False
 
 def start():
 	this.running = True
+	amadaa.connection.add_observer(amadaa.node.connection_change)
+	schedule.every(1).minutes.do(amadaa.connection.check_internet_connection)
 	while this.running:
 		schedule.run_pending()
 		time.sleep(1)
