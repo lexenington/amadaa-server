2d7389e159a62ea01c6becfaa91664769b561e1a
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-08 22:52:12 +0000

    moved amadaa.schema.* into amadaa.schema. Closes #41

--- amadaa/schema.py
+++ amadaa/schema.py
@@ -1,5 +1,22 @@
 import amadaa.database
 
+conn = amadaa.database.connection()
+
+print("checking if versions table exists")
+if not amadaa.database.table_exists('am_schema_version'):
+	with conn:
+		with conn.cursor() as cur:
+			cur.execute("""
+			create table am_schema_version(
+			schema_version_pk serial,
+			module varchar(40) not null,
+			version int not null,
+			primary key(schema_version_pk),
+			unique(module)
+			)""")
+			
+conn.close()
+
 def get_schema_version(module):
 	""" Get the current version of a module or None if the module is
 	not in the schema versions table. """
