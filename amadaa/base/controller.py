0157241f46edf30309e4f6a6b5f924a2fae6ef54
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-08 15:10:32 +0000

    pass template variables to Controller.render_template(). Closes #33

--- amadaa/base/controller.py
+++ amadaa/base/controller.py
@@ -5,7 +5,7 @@ class Controller:
 	def __init__(self):
 		self.view_dir = os.path.join(os.getcwd(), 'views')
 		
-	def render_template(self, tpl):
+	def render_template(self, tpl, template_vars={}):
 		env = Environment(loader = FileSystemLoader(self.view_dir))
 		t = env.get_template(tpl)
-		return t.render()
+		return t.render(template_vars)
