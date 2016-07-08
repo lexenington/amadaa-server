3fd116c5cd31fd3e35c9cda5d9a0825e2ef35230
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-08 22:08:09 +0000

    Added AuthController. closes #26

--- amadaa/auth.py
+++ amadaa/auth.py
@@ -17,3 +17,25 @@ def login_required(f):
 		else:
 			raise cherrypy.HTTPRedirect('/auth/login')
 	return decorate
+	
+class AuthController(Controller):
+	@cherrypy.expose
+	def login(self, username=None, password=None):
+		if username == None and password == None:
+			tvars = {}
+			if 'auth_message' in cherrypy.session:
+				tvars['auth_message'] = cherrypy.session.pop('auth_message')
+			return self.render_template('auth/login.html', tvars)
+		else:
+			uid = authenticate(username, password)
+			if uid == None:
+				cherrypy.session['auth_message'] = 'Authentication failed'
+				raise cherrypy.HTTPRedirect('/auth/login')
+			else:
+				cherrypy.session['user'] = uid
+				raise cherrypy.HTTPRedirect('/')
+		
+	@cherrypy.expose
+	def logout(self):
+		cherrypy.session.pop('user')
+		raise cherrypy.HTTPRedirect('/')
