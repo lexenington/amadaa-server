90bc08a1536559a5f0fe504df02513d93f147bf4
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-08 23:03:09 +0000

    Moved table creation into function in amadaa.setup. Closes #44

--- amadaa/schema.py
+++ amadaa/schema.py
@@ -1,21 +1,20 @@
 import amadaa.database
 
-conn = amadaa.database.connection()
-
-print("checking if versions table exists")
-if not amadaa.database.table_exists('am_schema_version'):
-	with conn:
-		with conn.cursor() as cur:
-			cur.execute("""
-			create table am_schema_version(
-			schema_version_pk serial,
-			module varchar(40) not null,
-			version int not null,
-			primary key(schema_version_pk),
-			unique(module)
-			)""")
-			
-conn.close()
+def setup():
+	conn = amadaa.database.connection()
+	print("checking if versions table exists")
+	if not amadaa.database.table_exists('am_schema_version'):
+		with conn:
+			with conn.cursor() as cur:
+				cur.execute("""
+				create table am_schema_version(
+				schema_version_pk serial,
+				module varchar(40) not null,
+				version int not null,
+				primary key(schema_version_pk),
+				unique(module)
+				)""")
+	conn.close()
 
 def get_schema_version(module):
 	""" Get the current version of a module or None if the module is
--- amadaa/setup.py
+++ amadaa/setup.py
@@ -1,5 +1,6 @@
 print("checking if schema versioning is installed")
 import amadaa.schema
+amadaa.schema.setup()
 	
 print("checking if user is installed")
 import amadaa.user.schema
