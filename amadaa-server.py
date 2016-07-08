8fb919a6cd4a8b3794873b8e6f3d8bfebd7cacd1
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-08 22:32:51 +0000

    Run amadaa.setup on server start. Closes #39

--- amadaa-server.py
+++ amadaa-server.py
@@ -4,6 +4,8 @@ import os
 import signal
 import amadaa.server
 
+import amadaa.setup
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 signal.signal(signal.SIGTERM, amadaa.server.sighandler)
 signal.signal(signal.SIGINT, amadaa.server.sighandler)
