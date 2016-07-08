d52ae8ffd41be63ad57e50411c56a315117a0d1e
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-08 11:18:24 +0000

    Added table_exists() to amadaa.database. Closes #18

--- amadaa/database.py
+++ amadaa/database.py
@@ -4,3 +4,15 @@ import psycopg2
 def connection():
 	# TODO: grab connection information from configuration file 
 	return psycopg2.connect("dbname=lorenzo user=lorenzo")
+
+def table_exists(table, schema='public'):
+	conn = connection()
+	with conn:
+		with conn.cursor() as cur:
+			sql = """select exists(
+				select 1 from information_schema.tables
+				where table_schema = %s and table_name = %s)"""
+			cur.execute(sql, (schema, table,))
+			ret = cur.fetchone()[0]
+	conn.close()
+	return ret
