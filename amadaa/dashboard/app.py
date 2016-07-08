eb9377ca4451aca156875316e6827bf06b06f64b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-08 22:26:28 +0000

    Added DashboardController. Closes #31

---
+++ amadaa/dashboard/app.py
@@ -0,0 +1,9 @@
+import cherrypy
+from amadaa.base import Controller
+from amadaa.auth import login_required
+
+class DashboardController(Controller):
+	@cherrypy.expose
+	@login_required
+	def index(self):
+		return self.render_template('dashboard/dashboard.html')
