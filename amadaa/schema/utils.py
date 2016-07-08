618942bacf88809aa6f9a5125a899e414f880408
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-08 13:31:06 +0000

    Created amadaa.schema.utils. Closes #22

---
+++ amadaa/schema/utils.py
@@ -0,0 +1,24 @@
+import amadaa.database
+
+def get_schema_version(module):
+	""" Get the current version of a module or None if the module is
+	not in the schema versions table. """
+	conn = amadaa.database.connection()
+	with conn:
+		with conn.cursor() as cur:
+			cur.execute("""select version from am_schema_version
+			where module = %s""", (module))
+			ret = cur.fetchone()
+	conn.close()
+	return ret
+
+def set_schema_version(module, version=1):
+	conn = amadaa.database.connection()
+	with conn:
+		with conn.cursor() as cur:
+			if version == 1:
+				cur.execute("""insert into am_schema_version(module, version)
+				values(%s, %s)""", (module, version))
+			else:
+				cur.execute("""update am_schema_version set version = %s
+				where module = %s""", (version, module))
