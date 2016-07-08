4b515dd98c677d010033c2c5f7901c5c34337cbe
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-08 15:35:53 +0000

    Create amadaa.base.model. Closes #19

---
+++ amadaa/base/model.py
@@ -0,0 +1,44 @@
+import uuid
+
+class Model:
+	def __init__(self):
+		self._attribs = { 'id': uuid.UUID }
+		self.id = None
+		
+	def __getattr__(self, key):
+		if key[0] == '_' or key in self._attribs:
+			# XXX: don't really know why I need this here, but at times
+			# I get stupid errors if I leave it out.
+			try:
+				return super.__getattr__(self, key)
+			except:
+				# TODO: appropriate exception class
+				raise Exception("get: no such key")
+		else:
+			# TODO: appropriate exception class
+			raise Exception("get: no such key")
+			
+	def __setattr__(self, key, value):
+		if key[0] == '_':
+			super.__setattr(self, key, value)
+		elif key in self._attribs:
+			if isinstance(value, self._attrib[key]) or value = None:
+				super.__setattr__(self, key, value)
+			else:
+				# TODO: appropriate exception class
+				raise Exception("set: incorrect value type")
+		else:
+			# TODO: appropriate exception class
+			raise Exception("set: no such attribute")
+			
+	def save():
+		if self.id == None:
+			self._insert()
+		else:
+			self._update()
+			
+	def _insert():
+		pass
+		
+	def _update():
+		pass
