8c45089042b9e21addb4ab2c7600a5d76060abcc
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-09 20:56:00 +0000

    Added amadaa.user.app.User.get(). Closes #61

--- amadaa/user/app.py
+++ amadaa/user/app.py
@@ -1,7 +1,10 @@
 import datetime
 import uuid
 import amadaa.database
+from psycopg2.extras import DictCursor, register_uuid
 from amadaa.base import Model
+
+register_uuid()
 
 class User(Model):
 	def __init__(self, id=None):
@@ -13,3 +16,15 @@ class User(Model):
 			'last_login': datetime,
 			'active': bool
 		})
+
+def get(self, id):
+		conn = amadaa.database.connection()
+		with conn:
+			with conn.cursor(cursor_factory=DictCursor) as cur:
+				cur.execute("""select * from am_user
+				where user_pk = %s""", (id,))
+				rec = cur.fetchone()
+				self.id = rec['user_pk']
+				self.username = rec['username']
+				self.password = rec['password']
+		conn.close()
