e3cf64ec7ba10492dc4db3387c9ddfbec209a3b0
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-09 20:50:15 +0000

    Added user fields to amadaa.user.app.User. Closes #59

--- amadaa/user/app.py
+++ amadaa/user/app.py
@@ -1,7 +1,15 @@
+import datetime
 import uuid
 import amadaa.database
 from amadaa.base import Model
 
 class User(Model):
 	def __init__(self, id=None):
-		super().__init__()	
+		super().__init__()
+		self._attribs.update({
+			'username': str,
+			'password': str,
+			'date_created': datetime,
+			'last_login': datetime,
+			'active': bool
+		})
