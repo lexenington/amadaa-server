caa368e13a4b3778395a5c6d712622d6d1679dd1
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-13 12:31:33 +0000

    Check if user schema exists in schema versions table before trying to create tables. Closes #84

--- amadaa/user/schema.py
+++ amadaa/user/schema.py
@@ -1,34 +1,35 @@
 import uuid
 import psycopg2.extras
 import amadaa.database
-from amadaa.schema import set_schema_version
+from amadaa.schema import set_schema_version, has_schema_version
 from amadaa.user.app import Role, RoleDirectory
 
-conn = amadaa.database.connection()
-if not amadaa.database.table_exists('am_role'):
-	with conn:
-		with conn.cursor() as cur:
-			cur.execute("""create table am_role(
-			role_pk uuid,
-			rolename varchar(40) not null,
-			parent_fk uuid references am_role,
-			primary key(role_pk),
-			unique(rolename))""")
+if not has_schema_version(__package__):
+	conn = amadaa.database.connection()
+	if not amadaa.database.table_exists('am_role'):
+		with conn:
+			with conn.cursor() as cur:
+				cur.execute("""create table am_role(
+				role_pk uuid,
+				rolename varchar(40) not null,
+				parent_fk uuid references am_role,
+				primary key(role_pk),
+				unique(rolename))""")
 			
-if not amadaa.database.table_exists('am_user'):
-	with conn:
-		with conn.cursor() as cur:
-			cur.execute("""
-			create table am_user(
-			user_pk uuid,
-			username varchar(30),
-			password varchar(40),
-			date_created timestamp,
-			last_login timestamp,
-			active bool, 
-			primary key(user_pk),
-			unique(username)
-			)""")
+	if not amadaa.database.table_exists('am_user'):
+		with conn:
+			with conn.cursor() as cur:
+				cur.execute("""
+				create table am_user(
+				user_pk uuid,
+				username varchar(30),
+				password varchar(40),
+				date_created timestamp,
+				last_login timestamp,
+				active bool, 
+				primary key(user_pk),
+				unique(username)
+				)""")
 			
 	set_schema_version(__package__, 1)
 
