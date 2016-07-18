4518c759c6afc8ca8703a8185e40d6528b9015b5
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-07-18 11:35:01 +0000

    Mounted UserAdminController. Closes #128

--- amadaa/server.py
+++ amadaa/server.py
@@ -9,6 +9,7 @@ import amadaa.node
 from amadaa.base import RootController
 from amadaa.auth import AuthController
 from amadaa.dashboard.app import DashboardController
+from amadaa.user.admin.app import UserAdminController
 
 this = sys.modules[__name__]
 this.running = False
@@ -20,34 +21,35 @@ ch.setFormatter(formatter)
 this.logger.addHandler(ch)
 
 def start():
-	this.running = True
-	this.logger.info('amadaa server starting')
-	amadaa.connection.add_observer(amadaa.node.connection_change)
-	schedule.every(1).minutes.do(amadaa.connection.check_internet_connection)
-	cherrypy.config.update({
-		'tools.sessions.on': True,
-		'tools.sessions.storage_type': 'ram'
-	})
-	cherrypy.tree.mount(RootController(), "/", {
-		'/static': {
-			'tools.staticdir.on': True,
-			'tools.staticdir.dir': os.path.join(os.getcwd(), 'static')
-		}
-	})
-	cherrypy.tree.mount(AuthController(), "/auth")
-	cherrypy.tree.mount(DashboardController(), "/dashboard")
-	cherrypy.engine.start()
-	this.logger.info('amadaa server started')
-	while this.running:
-		schedule.run_pending()
-		time.sleep(1)
-	stop()
-	
+    this.running = True
+    this.logger.info('amadaa server starting')
+    amadaa.connection.add_observer(amadaa.node.connection_change)
+    schedule.every(1).minutes.do(amadaa.connection.check_internet_connection)
+    cherrypy.config.update({
+        'tools.sessions.on': True,
+        'tools.sessions.storage_type': 'ram'
+    })
+    cherrypy.tree.mount(RootController(), "/", {
+        '/static': {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': os.path.join(os.getcwd(), 'static')
+        }
+    })
+    cherrypy.tree.mount(AuthController(), "/auth")
+    cherrypy.tree.mount(DashboardController(), "/dashboard")
+    cherrypy.tree.mount(UserAdminController(), "/admin/user")
+    cherrypy.engine.start()
+    this.logger.info('amadaa server started')
+    while this.running:
+        schedule.run_pending()
+        time.sleep(1)
+    stop()
+
 def stop():
-	this.logger.info('amadaa server shutting down')
-	cherrypy.engine.exit()
-	this.logger.info('amadaa server shutdown complete')
-	
+    this.logger.info('amadaa server shutting down')
+    cherrypy.engine.exit()
+    this.logger.info('amadaa server shutdown complete')
+
 def sighandler(signum, frame):
-	this.running = False
-	
+    this.running = False
+    
